a29a5f451c5853be0679f61a66b8c16fffb02501
Mirza Abdullah <99413332+MirzaAbdullah001@users.noreply.github.com>
2022-02-11 20:43:27 +0500

    commit

---
+++ Reversing Sentence.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// function prototype
+void reverse(const string& a);
+
+int main() {
+  string str;
+
+  cout << " Please enter a string " << endl;
+  getline(cin, str);
+    
+  // function call
+  reverse(str);
+
+  return 0;    
+}
+
+
+void reverse(const string& str) {
+  size_t numOfChars = str.size();
+
+  if(numOfChars == 1) {
+  cout << str << endl;}
+  else {
+    cout << str[numOfChars - 1];
+
+    // function recursion
+    reverse(str.substr(0, numOfChars - 1));
+  }
+
